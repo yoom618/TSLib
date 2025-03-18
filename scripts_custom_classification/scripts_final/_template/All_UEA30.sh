27e718e43abe71b3ef0163067d7d7efd072c59af
yoom618 <pamela7384@gmail.com>
2025-03-19 02:00:17 +0900

    add final script template

---
+++ scripts_custom_classification/scripts_final/_template/All_UEA30.sh
@@ -0,0 +1,65 @@
+# the best performing model for all UEA30 dataset
+# if there is more than one model, we choose the one with the lowest model size or computation cost
+data_dir="./dataset"
+checkpoint_dir="./checkpoints"
+gpu_id=0
+
+# ArticularyWordRecognition
+
+# AtrialFibrillation
+
+# BasicMotions
+
+# CharacterTrajectories
+
+# Cricket
+
+# DuckDuckGeese
+
+# EigenWorms
+
+# Epilepsy
+
+# ERing
+
+# EthanolConcentration
+
+# FaceDetection
+
+# FingerMovements
+
+# HandMovementDirection
+
+# Handwriting
+
+# Heartbeat
+
+# InsectWingbeat
+
+# JapaneseVowels
+
+# Libras
+
+# LSST
+
+# MotorImagery
+
+# NATOPS
+
+# PEMS-SF
+
+# PenDigits
+
+# PhonemeSpectra
+
+# RacketSports
+
+# SelfRegulationSCP1
+
+# SelfRegulationSCP2
+
+# SpokenArabicDigits
+
+# StandWalkJump
+
+# UWaveGestureLibrary
---
+++ scripts_custom_classification/scripts_final/_template/SpokenArabicDigits.sh
@@ -0,0 +1,20 @@
+data_dir="/data/yoom618/TSLib/dataset"
+checkpoint_dir="/data/yoom618/TSLib/checkpoints_best/DLinear"
+gpu_id=0
+
+# below all have the same performance
+
+python run.py \
+  --use_gpu True \
+  --gpu_type cuda \
+  --gpu ${gpu_id} \
+  --task_name classification \
+  --data UEA \
+  --root_path "${data_dir}/SpokenArabicDigits" \
+  --seq_len 93 \
+  --enc_in 13 \
+  --label_len 0 \
+  --pred_len 0 \
+  --c_out 0 \
+  --checkpoints ${checkpoint_dir} \
+  ...
